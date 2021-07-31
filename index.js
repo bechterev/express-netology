d7b7881b4cbc66b48b76bf9b4b497c4efd0f908f
bechterev <vitaly-bexterev@yandex.ru>
2021-07-31 16:30:49 +0400

    add comment

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const User = require('./models/user');
 const session = require('express-session');
+const http = require('http');
+const socketIO = require('socket.io');
 
 
 function verify(username, password, done) {
     User.findOne({login:username},function(err, user){
-      console.log(user,'23')
       if(user === null) {return done(null,false,{message:`user not found`});
       }
       if(bcrypt.compareSync(password, user.password)){
-        console.log('proverka good')
           return  done(null,user);        
       }
       else{
@@ -46,11 +46,12 @@ passport.use('local', new LocalStrategy(options, verify))
 
 // Конфигурирование Passport для сохранения пользователя в сессии
 passport.serializeUser(function (user, cb) {
+  console.log('ser', user.id)
    cb(null, user.id)
 })
 
 passport.deserializeUser(function (id, cb) {
-  
+  console.log(id, 'deser');
   User.findById(id).then(data=>{
     if(data===null) return cb(null);
     else return cb(null, id)
@@ -59,19 +60,32 @@ passport.deserializeUser(function (id, cb) {
 })
 
 const app = express();
-
+const server = http.Server(app);
+const io = socketIO(server);
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+const sessionMiddleware= session({
+  secret: 'user',
+  resave: false,
+  saveUninitialized: false,
+})
+app.use(sessionMiddleware)
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(session({
-  secret: 'user',
-  resave: false,
-  saveUninitialized: false,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
+io.use(wrap(sessionMiddleware));
+io.use(wrap(passport.initialize()));
+io.use(wrap(passport.session()))
+io.use((socket, next) => {
+  if (socket.request.user) {
+    next();
+  } else {
+    next(new Error('unauthorized'))
+  }
+});
 app.set('view engine', 'ejs');
 app.use(express.static("public/img"))
 app.use('/', indexRouter)
@@ -83,13 +97,35 @@ app.use('/books', bookRouter);
 app.use('/listuser', usersRouter);
 app.use('/api/book', bookApiRouter);
 app.use('/api/user', userApiRouter);
-
-
-
-
-
-
 app.use(cors());
+
+
+io.on('connection', (socket)=>{
+  const {id} = socket;
+  
+  socket.on('whoami', (cb) => {
+    cb(socket.request.user ? socket.request.user.username : '');
+  });
+
+  const session = socket.request.session;
+
+  session.socketId = socket.id;
+  session.save();
+    // работа с комнатами
+    const {roomName} = socket.handshake.query;
+    
+    socket.join(roomName);
+    socket.on('message-to-room', async (msg) => {
+        var user = await User.findById(socket.request.session.passport.user);
+        msg.username = user.login;
+        msg.type = `room: ${roomName}`;
+        socket.to(roomName).emit('message-to-room', msg);
+        socket.emit('message-to-room', msg);
+    });
+  socket.on('disconnect',(socket)=>{
+    console.log(`Socket disconnection ${id}`);
+  })
+})
 
 
 const PORT = process.env.PORT || 3000;
@@ -107,9 +143,9 @@ async function init() {
       useUnifiedTopology: true,
 
     })
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`app start from port ${PORT}`)
-    })
+    });
   }
   catch (err) {
     
